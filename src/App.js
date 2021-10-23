e422790b970ddacd1027119761c23591e066c577
RVirk04 <90662296+RVirk04@users.noreply.github.com>
2021-10-23 15:23:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Some Famous Star Wars Charaters</h1>
+      <h1>Some Famous Star Wars Characters</h1>
       <Data />
       <footer>Copyright @{date} - Randeep Virk</footer>
     </div>
